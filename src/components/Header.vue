290f09cfb7230b105c1554ecdef337a3215af359
obextrope <80216439+obextrope@users.noreply.github.com>
2021-03-07 11:56:46 -0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,7 +22,7 @@
       </span>
     </g-link>
     <div class="contact">
-      <a class="link" :href="`mailto:${data.email}`">say hi !</a>
+      <a class="link" :href="`mailto:${data.email}`">email button :⁾</a>
     </div>
   </header>
 </template>
