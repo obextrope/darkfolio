3c5be18a6295cab71a90bb5386c14a4641e64a38
obextrope <80216439+obextrope@users.noreply.github.com>
2021-03-12 21:54:53 -0500

    Update Project.vue

--- src/templates/Project.vue
+++ src/templates/Project.vue
@@ -131,7 +131,7 @@ p {
   position: relative;
   overflow: hidden;
   width: 100%;
-  padding-top: 75%; /* 4:3 Aspect Ratio */
+  padding-top: 100%; /* 1:1 Aspect Ratio */
 }
 
 /* Then style the iframe to fit in the container div with full height and width */
